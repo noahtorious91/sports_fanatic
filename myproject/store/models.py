0d6ed1e361e21b6584cee7ff2973377324037d4d
noahtorious91 <82792775+noahtorious91@users.noreply.github.com>
2025-04-29 22:51:53 -0700

    added homepage products list

--- myproject/store/models.py
+++ myproject/store/models.py
@@ -11,6 +11,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=10)
 
     def __str__(self):
         return self.name
--- myproject/store/views.py
+++ myproject/store/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Product
+import random
 
 def product_list(request):
     products = Product.objects.all()  # Query all products from the database
     return render(request, 'store/product_list.html', {'products': products})
 
 def home(request):
-    return render(request, 'store/home.html')
+    # Fetch all products and select a random subset
+    products = list(Product.objects.all())
+    random_products = random.sample(products, min(len(products), 5))  # Show up to 5 random products
+    return render(request, 'store/home.html', {'random_products': random_products})
