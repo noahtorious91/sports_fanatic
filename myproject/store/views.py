66cc5bbf4d0d5d7265d69474ddc552ce8320f5b2
noahtorious91 <82792775+noahtorious91@users.noreply.github.com>
2025-05-02 21:06:48 -0700

    got plp working and somewhat responsive

--- myproject/store/views.py
+++ myproject/store/views.py
@@ -1,13 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 import random
 
+# Query all products from the database - used on the product list page
 def product_list(request):
     products = Product.objects.all()  # Query all products from the database
     return render(request, 'store/product_list.html', {'products': products})
 
+# Query a random subset of products via the database - used on the home page
 def home(request):
     # Fetch all products and select a random subset
     products = list(Product.objects.all())
     random_products = random.sample(products, min(len(products), 5))  # Show up to 5 random products
     return render(request, 'store/home.html', {'random_products': random_products})
+
+# Gets the cart from the session and Gets the products in the cart
+def cart(request):
+    cart = request.session.get('cart', {})
+    products = Product.objects.filter(id__in=cart.keys())
+    cart_items = []
+    for product in products:
+        quantity = cart[str(product.id)]
+        total_price = product.price * quantity
+        cart_items.append({
+            'product': product,
+            'quantity': cart[str(product.id)]
+        })
+    return render(request, 'store/cart.html', {'cart_items': cart_items})
+
+# Add to cart function - gets the product ID from the request and gets the cart or an empty dictionary
+# and adds the product to the cart, incrementing the quanitity of items if it already exists
+def add_to_cart(request, product_id):
+    cart = request.session.get('cart', {}) 
+    cart[str(product_id)] = cart.get(str(product_id), 0) + 1
+    request.session['cart'] = cart
+    return redirect('cart')
+
+# Remove from cart function - gets the product ID fromt the request and gets the cart or an empty dictionary
+# and removes the product from the cart, deleting all the items and then saving the state of the cart. 
+def remove_from_cart(request, product_id):
+    cart = request.session.get('cart', {})
+    if str(product_id) in cart:
+        del cart[str(product_id)]
+    request.session['cart'] = cart
+    return redirect('cart')
+
+
+    
