f03a3facb252985b929bb7984105a2e55e79e9bd
noahtorious91 <82792775+noahtorious91@users.noreply.github.com>
2025-06-25 21:23:11 -0700

    added a bunch of styling to the catalog tool

--- myproject/store/models.py
+++ myproject/store/models.py
@@ -14,6 +14,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=10)
+    category = models.CharField(max_length=100, blank=True, null=True) 
     image = models.ImageField(upload_to='products/', blank=True, null=True)
     description = models.TextField(blank=True)
 
